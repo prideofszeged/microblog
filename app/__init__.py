5e6d3068a9664ea11c0c2c6b0cf9dde52a443cff
Steve D <stmada@gmail.com>
2018-07-27 06:23:17 -0500

    addint sqlalchemy stuff and migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-#__name__ = name of module
+# __name__ = name of module
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
